f30a40602025aee9664787e934967dc3f546512b
Jet Chiang <jetjiang.ez@gmail.com>
2024-12-06 01:05:31 -0500

    New layer class

--- include/layer.h
+++ include/layer.h
@@ -23,10 +23,18 @@ public:
      * Virtual function to perform backward propagation on input.
      * Must be overridden in derived class
      * 
-     * @param grad: The gradient of the loss function w.r.t. the output of this layer
+     * @param grad_output: The gradient of the loss function w.r.t. the output of this layer
      * @return: The gradient of the loss function w.r.t. the input of this layer
     */
     virtual Eigen::MatrixXf backward(const Eigen::MatrixXf& grad_output) = 0;
+
+    virtual bool has_weights() const { return false; }
+    virtual bool has_bias() const { return false; }
+
+    virtual Eigen::MatrixXf* get_weights() { return nullptr; }
+    virtual Eigen::MatrixXf* get_grad_weights() { return nullptr; }
+    virtual Eigen::VectorXf* get_bias() { return nullptr; }
+    virtual Eigen::VectorXf* get_grad_bias() { return nullptr; }
 
     // Virtual destructor to allow proper cleanup of derived classes
     virtual ~Layer() = default;
